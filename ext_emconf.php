394d933cc3130eb68955b46a7b692284c025e5e4
Benni Mack <benni@typo3.org>
2020-05-25 20:11:47 +0200

    [FEATURE] Add support for TYPO3 v11-dev

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,7 +12,7 @@ $EM_CONF[$_EXTKEY] = array(
     'version' => '10.0.0',
     'constraints' => array(
         'depends' => array(
-            'typo3' => '10.0.0-10.99.99',
+            'typo3' => '10.0.0-11.99.99',
         ),
         'conflicts' => array(
         ),
