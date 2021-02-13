762e31e6bbf874b6e4749f5e17ec187f241437a8
linawolf <48202465+linawolf@users.noreply.github.com>
2021-02-13 19:46:49 +0100

    Add new checkbox_16 to types showitem
    (cherry picked from commit 136378d3fa1615d1f638f88a8568e1b709294a8a)

--- Configuration/TCA/tx_styleguide_valuesdefault.php
+++ Configuration/TCA/tx_styleguide_valuesdefault.php
@@ -235,7 +235,7 @@ return [
                 --div--;basic,
                     input_1, input_2, input_3,
                     text_1,
-                    checkbox_1, checkbox_2, checkbox_3,
+                    checkbox_1, checkbox_2, checkbox_3, checkbox_16,
                     radio_1,
                 --div--;select,
                     select_1,select_2,
