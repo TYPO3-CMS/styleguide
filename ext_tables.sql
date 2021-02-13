c3e992825c6a5005b760e5678a8552d80a512f0e
linawolf <48202465+linawolf@users.noreply.github.com>
2021-02-13 18:53:37 +0100

    Added checkbox_16 example to valuesdefault

--- ext_tables.sql
+++ ext_tables.sql
@@ -699,6 +699,7 @@ CREATE TABLE tx_styleguide_valuesdefault (
 	checkbox_1 int(11) DEFAULT '0' NOT NULL,
 	checkbox_2 int(11) DEFAULT '0' NOT NULL,
 	checkbox_3 int(11) DEFAULT '0' NOT NULL,
+    checkbox_16 int(11) DEFAULT '0' NOT NULL,
 
 	radio_1 int(11) DEFAULT '0' NOT NULL,
 
