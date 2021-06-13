55f655ebdb4eb12af298a2a042609fef8b895b96
Jochen Roth <jochen.roth@b13.com>
2021-06-13 11:04:18 +0200

    [FIX] Remove DebuggerUtility

--- Classes/ViewHelpers/CodeViewHelper.php
+++ Classes/ViewHelpers/CodeViewHelper.php
@@ -16,7 +16,6 @@ namespace TYPO3\CMS\Styleguide\ViewHelpers;
  * The TYPO3 project - inspiring people to share!
  */
 
-use TYPO3\CMS\Extbase\Utility\DebuggerUtility;
 use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
 
 /**
