2b101d5b02b67f3fe21b8700f9ac90b03d63502d
Christian Kuhn <lolli@schwarzbu.ch>
2022-08-01 14:46:42 +0200

    [TASK] v11: Add PHP 8.2 to test matrix
    Update phpstan along the way:
    > composer req --dev phpstan/phpstan:^1.8.2
    > Build/Scripts/runTests.sh -s phpstanGenerateBaseline

--- Build/Scripts/runTests.sh
+++ Build/Scripts/runTests.sh
@@ -62,11 +62,12 @@ Options:
             - postgres: use postgres
             - sqlite: use sqlite
 
-    -p <7.4|8.0|8.1>
+    -p <7.4|8.0|8.1|8.2>
         Specifies the PHP minor version to be used
             - 7.4 (default): use PHP 7.4
             - 8.0: use PHP 8.0
             - 8.1: use PHP 8.1
+            - 8.2: use PHP 8.2
 
     -e "<phpunit, codeception or additional phpstan scan options>"
         Only with -s acceptance|functional|unit
